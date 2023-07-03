13b9d047967d3982aa3dd756dee6d3db31848e97
sebastian godirio <sebastianvagodirioo@yahoo.com.ar>
2023-07-02 21:24:09 -0300

    FEATURE/ la importacion de partidos ahora da los partidos recientes en un json

---
+++ Commands/DatosDeJuego/partido.js
@@ -0,0 +1,6 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+//const { newMatches } = require("../Utils/ImportarPartidos.js");
+
+
+// Aquí puedes utilizar la variable newMatches como se desee
+//console.log("Aquí están los nuevos partidos:", newMatches);
--- Commands/Utils/importarpartidos.js
+++ Commands/Utils/importarpartidos.js
@@ -9,12 +9,11 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ViewChannel),
 
   async execute(interaction) {
-    const url = "https://iosoccer-sa.bid/api/partidos";
-    const filePath = "ListasDePartidos/partidos.json";
-    const lobbyChannelId = "713573151971213322"; // Reemplaza con el ID del canal "lobby"
-
     try {
-      // Función para descargar y procesar el JSON
+      const url = "https://iosoccer-sa.bid/api/partidos";
+      const existingFilePath = "ListasDePartidos/partidos.json";
+      let counter = 0;
+
       const importarPartidos = async () => {
         try {
           // Realizar la solicitud HTTP y descargar el archivo JSON
@@ -24,7 +23,7 @@ module.exports = {
           // Leer el archivo JSON existente
           let existingData;
           try {
-            const existingDataString = fs.readFileSync(filePath, "utf8");
+            const existingDataString = fs.readFileSync(existingFilePath, "utf8");
             existingData = JSON.parse(existingDataString);
           } catch (error) {
             console.log("No se encontró el archivo JSON existente. Se creará uno nuevo.");
@@ -38,9 +37,24 @@ module.exports = {
             // Agregar los nuevos partidos al archivo JSON existente
             existingData.push(...newMatches);
 
-            // Guardar el archivo JSON actualizado
-            fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+            // Guardar el archivo JSON actualizado (sobrescribir el archivo existente)
+            fs.writeFileSync(existingFilePath, JSON.stringify(existingData, null, 2));
 
+            // Generar un archivo para los partidos recién actualizados
+            const newFilePath = `ListasDePartidos/partidos_Recientes.json`;
+
+            // Guardar los nuevos partidos en un archivo JSON diferente
+            fs.writeFileSync(newFilePath, JSON.stringify(newMatches, null, 2));
+
+            console.log("Se ha guardado el archivo JSON actualizado:", existingFilePath);
+            console.log("Se ha guardado el archivo JSON de los nuevos partidos:", newFilePath);
+          } else {
+            if (counter === 50) {
+              console.log("No hay nuevos partidos para importar.");
+              counter = 0; // Reiniciar el contador
+            } else {
+              counter++;
+            }
           }
         } catch (error) {
           console.error("Error al importar los partidos:", error);
@@ -52,9 +66,8 @@ module.exports = {
 
       // Establecer un temporizador para ejecutar la función cada 5 segundos
       const interval = setInterval(importarPartidos, 5000);
-
     } catch (error) {
-      console.error("Error al obtener partidos de forma automática:", error);
+      console.error("Error al importar los partidos:", error);
     }
   },
 };
